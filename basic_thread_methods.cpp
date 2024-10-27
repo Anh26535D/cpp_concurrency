7b5df10cda05b4b2b3140d07e6c9a824469974ce
Anh26535D <86581717+Anh26535D@users.noreply.github.com>
2024-10-27 17:22:26 +0700

    add basic_thread_methods

--- basic_thread_methods.cpp
+++ basic_thread_methods.cpp
@@ -3,6 +3,7 @@
 
 void foo() {
     std::cout << "Hello from function" << std::endl;
+    // get_id() returns the thread id
     std::cout << std::this_thread::get_id() << std::endl;
 }
 
@@ -10,27 +11,34 @@ class Bar {
 public:
     void operator()() {
         std::cout << "Hello from callable class" << std::endl;
+        // get_id() returns the thread id
         std::cout << std::this_thread::get_id() << std::endl;
     }
 };
 
 int main() {
+    // We can create a thread by passing a function, a callable object, or a lambda expression
+    // A thread is joinable if it is not joined or detached
+    // A thread is not joinable if it is joined or detached
+    // If we don't join or detach a thread, the program will terminate when the thread goes out of scope
     std::thread thread1(foo);
     if (thread1.joinable()) {
         std::cout << "Thread 1 is joinable" << std::endl;
-        thread1.join();
+        thread1.join(); // The main thread waits for thread1 to finish
     }
-    
     
     Bar obj;
     std::thread thread2(obj);
     if (thread2.joinable()) {
         std::cout << "Thread 2 is joinable" << std::endl;
-        thread2.join();
+        // The main thread doesn't wait for thread2 to finish
+        // Thread2 runs independently
+        thread2.detach(); 
     }
 
     std::thread thread3([] {
         std::cout << "Hello from lambda" << std::endl;
+        // get_id() returns the thread id
         std::cout << std::this_thread::get_id() << std::endl;
     });
     if (thread3.joinable()) {
